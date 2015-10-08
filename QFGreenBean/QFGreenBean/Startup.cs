ed04414e6b5cbeb0b6d9eaa93428e2e0b37a8dd0
layheng <henglay@hotmail.com>
2015-10-08 18:03:52 -0400

    First release
    First release.

---
+++ QFGreenBean/QFGreenBean/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(QFGreenBean.Startup))]
+namespace QFGreenBean
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
