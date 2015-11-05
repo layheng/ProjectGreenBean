4964f10805b4bff67d9d6560b2868239e85590ce
layheng <henglay@hotmail.com>
2015-11-05 15:34:42 -0500

    Add StudentScheduleGenerator model

---
+++ QFGreenBean/QFGreenBean/Models/StudentScheduleGenerator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QFGreenBean.Models
+{
+    public class StudentScheduleGenerator
+    {
+        public string StudentNumber { get; set; }
+
+        public string StartingSemester { get; set; }
+
+        public string Department { get; set; }
+
+        public string ProgramOption { get; set; }
+
+        public bool IncludeSummer { get; set; }
+    }
+}
